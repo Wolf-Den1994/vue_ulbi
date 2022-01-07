552f72a25c44a93fafe4e93f768b8786a9b88ab6
Denis <wolf-den@tut.by>
2022-01-07 14:37:51 +0300

    add post form + two buiding + v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
+  <div class="app">
+    <form>
+      <h4>Создание постав</h4>
+      <input
+        :value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Название"
+      >
+      <input
+        :value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      >
+      <button
+        class="btn"
+        @click="createPost"
+      >Создать</button>
+    </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название:</strong> {{ post.title }}</div>
       <div><strong>Описание:</strong> {{ post.body }}</div>
@@ -15,10 +36,20 @@ export default {
         { id: 1, title: 'JavaScript 1', body: 'Описание поста 1' },
         { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
         { id: 2, title: 'JavaScript 3', body: 'Описание поста 3' },
-      ]
+      ],
+      title: '',
+      body: '',
     }
   },
   methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost)
+    },
   }
 }
 </script>
@@ -30,9 +61,34 @@ export default {
   box-sizing: border-box;
 }
 
+.app {
+  padding: 20px;
+}
+
 .post {
   padding: 15px;
   border: 2px solid teal;
   margin-top: 15px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
 }
 </style>
