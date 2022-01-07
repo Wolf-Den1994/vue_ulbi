74792e53f91166c9bbfbcc2adc0470b4bce77e66
Denis <wolf-den@tut.by>
2022-01-07 15:27:38 +0300

    global component (add myInput)

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -3,18 +3,16 @@
     @submit.prevent="createPost"
   >
     <h4>Создание постав</h4>
-    <input
+    <my-input
       v-model="post.title"
-      class="input"
       type="text"
       placeholder="Название"
-    >
-    <input
+    />
+    <my-input
       v-model="post.body"
-      class="input"
       type="text"
       placeholder="Описание"
-    >
+    />
     <my-button
       style="align-self: flex-end; margin-top: 15px"
     >
@@ -50,12 +48,5 @@ export default {
 form {
   display: flex;
   flex-direction: column;
-}
-
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
 }
 </style>
