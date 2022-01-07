df404dbf554e445eb74550dd01cf609c173c9d9a
Denis <wolf-den@tut.by>
2022-01-07 15:58:02 +0300

    modalka

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="app">
-    <post-form @create="createPost" />
+    <h1>Страница с постами</h1>
+    <my-button
+      @click="showDialog"
+      style="margin: 15px 0"
+    >
+      Создать пользователя
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
     <post-list
       :posts="posts"
       @remove="removePost"
@@ -20,14 +29,19 @@ export default {
         { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
         { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' },
       ],
+      dialogVisible: false,
     }
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter(p => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
     }
   },
   components: {
