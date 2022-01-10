efd483cc522a7a8ab6925c454913bfdd31e93038
Denis <wolf-den@tut.by>
2022-01-10 21:23:09 +0300

    pagination

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,22 @@
       v-if="!isPostsLoading"
     />
     <div v-else>Идет загрузка...</div>
+    <div class="page__wrapper">
+      <div
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        class="page"
+        :class="{
+          'current-page': page === pageNumber
+        }"
+        :style="{
+          color: page === pageNumber ? 'teal' : ''
+        }"
+        @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,6 +57,9 @@ export default {
       isPostsLoading: false,
       selectedSort: '',
       searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
       sortOptions: [
         {value: 'title', name: 'По названию'},
         {value: 'body', name: 'По описанию'},
@@ -58,10 +77,19 @@ export default {
     showDialog() {
       this.dialogVisible = true;
     },
+    changePage(pageNumber) {
+      this.page = pageNumber;
+    },
     async fetchPosts() {
       try {
         this.isPostsLoading = true
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit,
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
         this.posts = response.data
       } catch (e) {
         alert('Ошибка')
@@ -75,11 +103,15 @@ export default {
     this.fetchPosts()
   },
   watch: {
+    // тоже самое написано в computed -> sortedPost
     // selectedSort(newValue) {
     //   this.posts.sort((post1, post2) => {
     //     return post1[newValue]?.localeCompare(post2[newValue])
     //   })
-    // }
+    // },
+    page () {
+      this.fetchPosts();
+    }
   },
   computed: {
     sortedPosts() {
@@ -111,5 +143,19 @@ export default {
   margin: 15px 0;
   display: flex;
   justify-content: space-between;
+}
+
+.page {
+  border: 1px solid #000;
+  padding: 10px;
+}
+
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+
+.current-page {
+  border: 2px solid teal;
 }
 </style>
