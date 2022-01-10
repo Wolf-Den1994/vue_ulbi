934dd8d257e09cd7cbc36bb40709caa015d675f2
Denis <wolf-den@tut.by>
2022-01-10 20:57:09 +0300

    add animation transition group

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,12 +2,16 @@
 <!--  v-show="posts.length"  -->
   <div v-if="posts.length">
     <h3>Список пользователей</h3>
-    <post-item
-      v-for="post in posts"
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group
+      name="post-list"
+    >
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <!--  v-show="!posts.length"  -->
   <h2
@@ -33,5 +37,20 @@ export default {
 </script>
 
 <style scoped>
-
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.3s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(150px);
+}
+.post-list-move {
+  transition: transform 0.3s ease;
+}
 </style>
